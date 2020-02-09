c36f1d7f1c775d55d7dc51567c0738f30f653fc3
avgre <avtar.g10@gmail.com>
2020-02-09 16:33:52 -0500

    initial commit

---
+++ server/data.js
@@ -0,0 +1,25 @@
+const games = [
+  {
+    name: 'Red Light, Green Light',
+    img: '/images/redlight.jpg',
+    desc: [
+      'Red means stop. Green means go. But you gotta hurry before time runs out!',
+    ],
+    id: 0,
+  },
+  {
+    name: 'The Floor is Lava',
+    img: '/images/floorlava.jpeg',
+    desc: [
+      'Only your furniture can save you from the hot, bubbling, molten lava!',
+    ],
+    id: 1,
+  },
+  {
+    name: 'Musical Chairs',
+    img: '/images/music.jpg',
+    desc: ['Now is your chance to show off those sweet dance moves!'],
+    id: 2,
+  },
+];
+module.exports = { games };
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const data = require('./data.js');
+const games = data.games;
+
+app.get('/api/games', (req, res) => {
+  res.send(
+    JSON.stringify({
+      success: true,
+      games: games,
+    })
+  );
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/hello', (req, res) => res.json({ data: 'World!' }));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
