8ab3a85799355cb94e8708a619d6700a75ea7b3c
avgre <avtar.g10@gmail.com>
2021-05-22 14:31:42 -0400

    Update Settings Component -fetchLights and fetchIP -recieve props from toggle for light status -Add Modal and Toggles

--- client/src/components/settings.jsx
+++ client/src/components/settings.jsx
@@ -1,56 +1,166 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import { keyframes } from 'styled-components';
+import { ReactComponent as Hub } from '../images/hub.svg';
+import Modal from './modal.js';
+import LightControl from './settingsLight.js';
 
-const StyledSettings = styled('div')`
-  position: relative;
-  background: #512da8ff;
-  z-index: 1;
-  width: 100%;
-  height: 92vh;
-`;
+// async function fetchLights() {
+//   let response = await fetch(
+//     'https://' + this.props.hubIp + '/api/' + this.props.user + '/lights'
+//   );
+//   console.log('response', response);
+//   if (!response.ok) {
+//     throw new Error('Network request failed');
+//   }
+//   let bridgeLights = await response.json();
+//   return bridgeLights;
+// }
+
+// async function fetchIP() {
+//   let response = await fetch('https://discovery.meethue.com/');
+//   if (!response.ok) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   }
+//   let bridgeIP = await response.json();
+//   return bridgeIP;
+// }
 
 class Settings extends Component {
   constructor(props) {
     super(props);
-    this.hubURL =
-      'https://192.168.12.100/api/-BHo-XWLWKff53wesEVS8BrKp7QkVUJTn3TEGCrd/lights/3/state';
-    this.state = { hueip: null };
+
+    this.state = {
+      lights: {},
+      show: false,
+      responseFailed: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
+  showModal = () => {
+    this.setState({ show: true });
+    console.log(this.state);
+  };
+  hideModal = () => {
+    this.setState({ show: false });
+    console.log(this.state);
+  };
+
   componentDidMount() {
-    fetch('https://discovery.meethue.com/')
+    if (this.props.user === undefined) this.fetchIP();
+  }
+  fetchLights = async (ip) => {
+    console.log('fetchLights called');
+    let response = await fetch(
+      'https://' + ip + '/api/' + this.props.user + '/lights'
+    );
+
+    if (!response.ok) {
+      throw new Error('Network request failed');
+    }
+    let bridgeLights = await response.json();
+    console.log(bridgeLights);
+    this.props.dispatch({
+      type: 'SET_LIGHTS',
+      payload: bridgeLights,
+    });
+  };
+  fetchIP = async () => {
+    console.log('fetchIP called');
+    const response = await fetch('https://discovery.meethue.com/');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const bridgeIP = await response.json();
+    this.props.dispatch({
+      type: 'SET_BRIDGE_IP',
+      payload: bridgeIP[0].internalipaddress,
+    });
+
+    this.fetchLights(bridgeIP[0].internalipaddress);
+  };
+  componentDidUpdate() {}
+  checkHueData = () => {
+    if (this.props.user && this.props.hubIp) {
+    }
+  };
+  createUser = () => {
+    fetch('https://' + this.props.hubIp + '/api/', {
+      method: 'POST',
+      body: '{"devicetype":"games_with_hue#browser"}',
+    })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data[0].internalipaddress);
-        this.props.dispatch({
-          type: 'SET_BRIDGE_IP',
-          payload: data[0].internalipaddress,
-        });
+        if (data[0].error) {
+          console.log('error has occured', data);
+        } else {
+          this.props.dispatch({
+            type: 'SET_USER',
+            payload: data[0].success.username,
+          });
+        }
       });
-  }
-  changeLightColor(hubUrl) {
-    console.log(this.hubURL + 'lights/3/state');
-    fetch(hubUrl, {
-      method: 'PUT',
-      body: '{"transitiontime":' + 0 + ',"on":' + false + '}',
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-    return;
-  }
-  connectHub(hubUrl) {
-    console.log(this.hubURL + 'lights/3/state');
-    fetch(hubUrl, {
-      method: 'PUT',
-      body: '{"transitiontime":' + 0 + ',"on":' + false + '}',
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-    return;
-  }
+    this.fetchLights(this.props.hubIp);
+  };
+
+  log = () => {
+    console.log(this.state);
+  };
+
   render() {
+    const onchange = (data) => {
+      this.setState({ data });
+      console.log('Form>', data);
+    };
+    const data = this.props.lights;
+    const lights = [];
+
+    if (this.props.lights.length === 1) {
+      console.log('error');
+    } else {
+      Object.keys(data).forEach(function (id, index) {
+        const item = data[id];
+        const light = (
+          <LightControl
+            onchange={(e) => {
+              onchange(e);
+              console.log(id, e);
+            }}
+            key={id}
+            id={id}
+            name={data[id].name}
+            isOn="false"
+            toggled={false}
+            bri={item.state.bri}
+          />
+        );
+
+        lights.push(light);
+      });
+    }
     return (
       <StyledSettings>
+        <button type="button" onClick={this.showModal}></button>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <form>
+            <label>
+              Hue Bridge IP Address:
+              <input type="text" name="Bridge IP" value={this.props.hubIp} />
+            </label>
+            <label>
+              Hue Bridge Username:
+              <input type="text" name="Bridge User" value={this.props.user} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </Modal>
+        <AnimationDiv>
+          <BlueBulbie />
+          <Hub />
+        </AnimationDiv>
+        <button onClick={this.createUser}>Set User</button>
         <span>
           Click <a href="https://discovery.meethue.com/">here</a> to find your
           IP
@@ -62,19 +172,49 @@ class Settings extends Component {
           </label>
           <input type="submit" value="Submit" />
         </form>
-        <span>Ip sent to redux store on submit</span>
-        <span>Use ip to to access debugger</span>
-        <button onClick={this.changeLightColor(this.hubURL)}>Click</button>
+
+        <button onClick={this.log}>log</button>
+
+        <span> Name: {this.props.user}</span>
+        <div>{lights}</div>
       </StyledSettings>
     );
   }
 }
+
 const mapStateToProps = (state) => {
   return {
     hubIp: state.hubAddress,
     user: state.hubUsername,
-    currentColor: state.currentColor,
+    lights: state.lights,
   };
 };
 
 export default connect(mapStateToProps)(Settings);
+
+const jump = keyframes`
+100% {
+  background-position: 700px;
+}
+`;
+const BlueBulbie = styled('div')`
+  z-index: 5;
+  width: 100px;
+  height: 140px;
+  background-image: url('/images/bulbie-jump.svg');
+  animation: ${jump} 1s steps(7) infinite;
+`;
+
+const AnimationDiv = styled('div')`
+  position: relative;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledSettings = styled('div')`
+  position: relative;
+  background: #512da8ff;
+  z-index: 1;
+  width: 100%;
+  height: 92vh;
+`;
