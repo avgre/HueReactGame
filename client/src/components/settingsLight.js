27e047f610b8403b9de203a00ea0f441520dbe79
avgre <avtar.g10@gmail.com>
2021-05-26 16:50:38 -0400

    Config Toggle and Light Settings Style, Add Btn

--- client/src/components/settingsLight.js
+++ client/src/components/settingsLight.js
@@ -1,72 +1,87 @@
 import React, { useState } from 'react';
 import Toggle from './toggle.js';
 import styled from 'styled-components';
+import Button from './button3d.js';
 
 function LightControl(props) {
   const [isToggled, setIsToggled] = useState(false);
-
+  const [isClicked, setIsClicked] = useState(null);
   const handleChange = (event) => {
     props.onchange(event);
   };
+  const handleSelect = (status) => {
+    if (status) {
+      return 'primary';
+    } else return 'secondary';
+  };
   return (
-    <Flex>
-      <Light>
-        <Title>{props.name}</Title>
+    <Light>
+      <Title>{props.name}</Title>
 
+      {/* <Toggle
+        id={props.id}
+        onChange={(e) => {
+          setIsToggled(e.target.checked);
+          handleChange(e.target.checked);
+        }}
+      /> */}
+      <Button
+        onClick={(e) => {
+          setIsClicked(!isClicked);
+          handleChange(props.id);
+        }}
+        chosen={props.isSet}
+        type={handleSelect(props.setLight)}
+      >
+        Connect light
+      </Button>
+      <ControlDiv>
         <Toggle
-          id={props.id}
+          id={'t2' + props.id}
+          toggled={props.isOn}
           onChange={(e) => {
             setIsToggled(e.target.checked);
-            handleChange(e.target.checked);
+            props.onToggleLight(props.id, props.isOn);
           }}
         />
-        <ControlDiv>
-          <Toggle
-            id={'t2' + props.id}
-            toggled={props.isOn}
-            onChange={(e) => {
-              setIsToggled(e.target.checked);
-              props.onToggleLight(props.id, props.isOn);
-            }}
-          />
-          <input
-            id={'slider' + props.id}
-            type="range"
-            value={props.bri}
-            min="0"
-            max="256"
-            step={10}
-            onChange={(event, newValue) => {
-              props.onBrightnessChanged(props.id, event.target.value);
-            }}
-          />
-        </ControlDiv>
-      </Light>
-    </Flex>
+        <input
+          id={'slider' + props.id}
+          type="range"
+          value={props.bri}
+          min="0"
+          max="256"
+          step={10}
+          onChange={(event, newValue) => {
+            props.onBrightnessChanged(props.id, event.target.value);
+          }}
+        />
+      </ControlDiv>
+    </Light>
   );
 }
 
 export default LightControl;
 
 const Light = styled('div')`
-  width: 350px;
+  flex: 0 0 50%;
+  max-width: 360px;
   height: 200px;
   background: #322290;
   border-radius: 15px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: 40px;
 `;
+
 const Flex = styled('div')`
   flex: 0 0 50%;
   height: 250px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 const ControlDiv = styled('div')`
   display: flex;
 `;
-const Title = styled('h4')`
+const Title = styled.span`
   color: white;
 `;
--- client/src/components/toggle.js
+++ client/src/components/toggle.js
@@ -1,46 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SwitchInput = styled.input`
-  height: 0;
-  width: 0;
-  visibility: hidden;
-`;
-
-const SwitchLabel = styled.label`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  width: 80px;
-  height: 40px;
-  border-radius: 100px;
-  border: 2px solid gray;
-  position: relative;
-  transition: background-color 0.2s;
-`;
-
-const SwitchButton = styled.span`
-  content: '';
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  width: 36px;
-  height: 36px;
-  border-radius: 45px;
-  transition: 0.2s;
-  background: grey;
-  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
-  ${SwitchInput}:checked + ${SwitchLabel} & {
-    left: calc(100% - 2px);
-    transform: translateX(-100%);
-  }
-
-  ${SwitchLabel}:active & {
-    width: 45px;
-  }
-`;
-
 const Switch = ({ id, toggled, onChange }) => {
   return (
     <>
@@ -60,3 +20,43 @@ const Switch = ({ id, toggled, onChange }) => {
 };
 
 export default Switch;
+
+const SwitchInput = styled.input`
+  height: 0;
+  width: 0;
+  visibility: hidden;
+`;
+
+const SwitchLabel = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  width: 55px;
+  height: 33px;
+  border-radius: 100px;
+  border: 2px solid #2196f3;
+  position: relative;
+  transition: background-color 0.2s;
+`;
+
+const SwitchButton = styled.span`
+  content: '';
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  width: 25px;
+  height: 25px;
+  border-radius: 33px;
+  transition: 0.2s;
+  background: #2196f3;
+  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
+  ${SwitchInput}:checked + ${SwitchLabel} & {
+    background: #53a147;
+    left: calc(100% - 2px);
+    transform: translateX(-100%);
+  }
+  ${SwitchLabel}:active & {
+    width: 45px;
+  }
+`;
